186afe219901b36dc092a59a637407a67895b710
lucas lima <lucaslj@ufba.br>
2023-07-04 10:31:55 -0300

    add request calls

--- jwtapp/lib/Views/Pages/login_page.dart
+++ jwtapp/lib/Views/Pages/login_page.dart
@@ -18,17 +18,22 @@ class _LoginPageState extends State<LoginPage> {
 
   Future<Map<String, dynamic>> loginUser(
       String username, String password) async {
-    const apiUrl = 'https://minimalapaijwt.azurewebsites.net/login';
+    const apiUrl = 'https://minimalapijwt.azurewebsites.net/login';
+    const localhostUrl = 'http://192.168.1.103:5127/login';
+    print("LOGINUSER");
 
     final response = await http.post(
-      Uri.parse(apiUrl),
+      Uri.parse(localhostUrl),
       body: {
         'username': username,
         'password': password,
       },
     );
+    print("LOGINUSER");
 
     if (response.statusCode == 200) {
+      print("STATUSCODE200");
+
       final responseData = json.decode(response.body);
       final token = responseData['token'];
       final id = responseData['user']['id'];
@@ -45,89 +50,97 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void _handleLogin() async {
-    if (_formKey.currentState!.validate()) {
-      final username = _userNameFormController.text;
-      final password = _passwordFormController.text;
+    print("HANDLElOGIN");
+    // if (_formKey.currentState!.validate()) {
+    final username = _userNameFormController.text;
+    final password = _passwordFormController.text;
+    print(
+        'usernamecontroller: ${_userNameFormController.text} \n usernamecontroller: ${_passwordFormController.text}');
+    try {
+      print("TRY");
+      final loginData = await loginUser(username, password);
+      final token = loginData['token'];
+      final id = loginData['id'];
+      final role = loginData['role'];
 
-      try {
-        final loginData = await loginUser(username, password);
-        final token = loginData['token'];
-        final id = loginData['id'];
-        final role = loginData['role'];
+      print('Token: $token');
+      print("\n");
+      print('ID: $id');
+      print("\n");
+      print('Role: $role');
+      print("\n");
+      print(
+          'usernamecontroller: ${_userNameFormController.text} \n usernamecontroller: ${_passwordFormController.text}');
 
-        print('Token: $token');
-        print("\n");
-        print('ID: $id');
-        print("\n");
-        print('Role: $role');
-        print("\n");
-
-        // Faça algo com o token, id e role, como navegar para a próxima tela ou salvar em um estado global.
-      } catch (e) {
-        print('Error: $e');
-        // Trate o erro de login, como exibindo uma mensagem de erro.
-      }
+      // Faça algo com o token, id e role, como navegar para a próxima tela ou salvar em um estado global.
+    } catch (e) {
+      print('Error: $e');
+      // Trate o erro de login, como exibindo uma mensagem de erro.
+      // }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Center(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: _userNameFormController,
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      hintText: "Digite seu nome",
-                      border: const OutlineInputBorder(),
-                      filled: true,
-                      focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Theme.of(context).primaryColor),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.secondary),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  TextFormField(
-                    controller: _passwordFormController,
-                    keyboardType: TextInputType.name,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Digite sua senha",
-                      border: const OutlineInputBorder(),
-                      filled: true,
-                      focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Theme.of(context).primaryColor),
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.secondary),
-                        borderRadius: BorderRadius.circular(4),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Center(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: _userNameFormController,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        hintText: "Digite seu nome",
+                        border: const OutlineInputBorder(),
+                        filled: true,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Theme.of(context).primaryColor),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.secondary),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: _handleLogin,
-                    child: const Text("LOGIN"),
-                  ),
-                ],
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      controller: _passwordFormController,
+                      keyboardType: TextInputType.name,
+                      // obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Digite sua senha",
+                        border: const OutlineInputBorder(),
+                        filled: true,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Theme.of(context).primaryColor),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.secondary),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: _handleLogin,
+                      child: const Text("LOGIN"),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
